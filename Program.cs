477fa69147eea26919bc8b08ec6bcfc620f9fdf7
KiTaec <school37aleksei@gmail.com>
2023-09-14 13:09:57 +0300

    Z

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ты хочешь сахар в чай?");
+string txt = Console.ReadLine();
+if (txt == "Yes")
+{
+    Console.WriteLine("Сколько ложек сахара ты хочешь?");
+    int sum = Convert.ToInt32(Console.ReadLine());
+    int i = sum;
+
+        Console.WriteLine("Добавили " + i + " ложек сахара");
+
+
+}
